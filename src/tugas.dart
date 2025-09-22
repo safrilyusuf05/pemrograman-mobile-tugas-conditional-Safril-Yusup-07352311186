63b3931c2dbbb99551fa3b09a0b7197b21c36fd1
safrilyusuf05 <safrilyusuf05@gmail.com>
2025-09-22 21:26:39 +0900

    Update tugas.dart

--- src/tugas.dart
+++ src/tugas.dart
@@ -1,1 +1,35 @@
+import 'dart:io';
+void main() {
+  stdout.write("Masukkan skor Anda (0-100): ");
+  String? input = stdin.readLineSync();
+
+  int? skor = int.tryParse(input ?? "");
+
+  if (skor == null) {
+    print("Error: Input bukan angka.");
+    return;
+  }
+
+  if (skor < 0 || skor > 100) {
+    print("Error: Skor harus berada dalam rentang 0 - 100.");
+    return;
+  }
+
+  String grade;
+
+  if (skor >= 85 && skor <= 100) {
+    grade = "A";
+  } else if (skor >= 70 && skor <= 84) {
+    grade = "B";
+  } else if (skor >= 60 && skor <= 69) {
+    grade = "C";
+  } else if (skor >= 50 && skor <= 59) {
+    grade = "D";
+  } else {
+    grade = "E";
+  }
+
+  print("Skor Anda: $skor");
+  print("Grade Anda: $grade");
+}
 
